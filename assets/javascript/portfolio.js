f2e51e389fbef929861da4157dd6d424894f99c3
oscar luna <os.lun11@gmail.com>
2017-08-24 21:58:15 -0700

    javascript almost complete

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -16,5 +16,43 @@ $(document).ready(function () {
         $(this).text("Less...");
     });
 
+    $(document).on("scroll", onScroll);
 
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('.sidebar-navigation ul li a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('.sidebar-navigation ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else {
+                currLink.removeClass("active");
+            }
+        });
+    }
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    })
 });
